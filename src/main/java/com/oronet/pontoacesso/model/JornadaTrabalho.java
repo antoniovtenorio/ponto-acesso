398bbbf038c43dda39fff24562b77da426777df5
Antonio Tenorio <antonio.v.tenorio@outlook.com>
2021-07-29 00:44:06 -0300

    Created Service, Repository, Controller to Jornada and Swagger Schema.

--- src/main/java/com/oronet/pontoacesso/model/JornadaTrabalho.java
+++ src/main/java/com/oronet/pontoacesso/model/JornadaTrabalho.java
@@ -5,6 +5,7 @@ import org.hibernate.envers.Audited;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Data
@@ -15,7 +16,7 @@ import javax.persistence.Id;
 @Audited
 public class JornadaTrabalho {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String descricao;
 }
